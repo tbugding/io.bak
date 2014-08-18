cbac813f732eec4eb423898ed74fa136340fa8b4
m-g- <1121506496@qq.com>
2014-08-18 14:36:54 +0800

    加了动画

--- work/obdsite2/js/common.js
+++ work/obdsite2/js/common.js
@@ -1,3 +1,5 @@
+
+//获取屏幕宽高和用户代理
 var client = {
 	width : 0,
 	height : 0,
@@ -10,16 +12,6 @@ function getClient(){
 	client.UA = window.navigator.userAgent;
 	// console.log('width: ' + client.width + '\n' + 'height: ' + client.height + '\n' +'UA: ' + client.UA);
 	document.title = client.width +' - '+ client.height;
-
-	var nav = $('.nav');
-	// $('.nav_btn').on('click',function(){
-	// 	alert('0')
-	// 	nav.toggleClass('show')
-	// })
-	$('.nav_btn').get(0).onclick=function(){
-		
-		nav.toggleClass('show')
-	}
 }
 $(window).on('resize',getClient);
 
@@ -27,4 +19,9 @@ $(window).on('resize',getClient);
 
 $(function(){
 	getClient();
+	var nav = $('.nav');
+	//原生click事件，JQ事件发现一次点击触发多次事件bug
+	$('.nav_btn').get(0).onclick=function(){
+		nav.toggleClass('show');
+	}
 })
--- work/obdsite2/js/index.js
+++ work/obdsite2/js/index.js
@@ -1,8 +1,32 @@
 $(function(){
-	$('#fullpage').fullpage({
+	var fullpage = $('#fullpage');
+	fullpage.fullpage({
 		verticalCentered: false,
 		easing: 'easeOutQuint',
 		css3: true,
-		navigation: true
+		navigation: true,
+		afterRender: function(){
+			setTimeout(function(){
+				$('.banner_logo').addClass('animate')
+			},100);
+			setTimeout(function(){
+				$('.banner_t1').addClass('animate')
+			},300);
+			setTimeout(function(){
+				$('.banner_t2').addClass('animate')
+			},500);
+			setTimeout(function(){
+				$('.banner_t3').addClass('animate')
+			},700);
+			setTimeout(function(){
+				$('.banner_bg_black').addClass('animate')
+			},900);
+			setTimeout(function(){
+				$('.banner_bg').addClass('animate')
+			},900);
+		}
 	})
+	// fullpage.on('afterRender',function(){
+	// 	alert('ok')
+	// })
 })
