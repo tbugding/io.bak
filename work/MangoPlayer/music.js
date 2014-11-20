980e8606be42b0250a1d92c733582a85ea264be8
Mango <1121506496@qq.com>
2014-11-20 22:30:03 +0800

    updata

--- work/MangoPlayer/music.js
+++ work/MangoPlayer/music.js
@@ -1,69 +1,71 @@
-
+(function(aCanvas,mp3){
+	//audio 上下文
 	window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext;
+	if(!AudioContext){return;}
 
 
-	if (typeof window.AudioContext == "undefined") {
-		document.getElementById("show").innerHTML = '<object id="player" classid="CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6"> <param name="url" value="' + a + '.mp3" /><param name="AutoStart" value="1" /><embed src="' + a + '.mp3" type="application/x-mplayer2" width="360" height="60" autostart="true"></embed></object>'
-	} else {
-		var n = new AudioContext(),
-		r = n.createAnalyser(),
-		// t = new Audio("http://www.wuweierwei.com/music/a.mp3"),
-		t = new Audio("02.mp3"),
-		k = n.createMediaElementSource(t),
-		j = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame,
-		l = r.frequencyBinCount / 2,
-		u = new Uint8Array(l),
-		f = document.querySelector("#canvas"),
-		f2 = document.querySelector("#canvas2"),
-		p = f.getContext("2d"),
-		p2 = f2.getContext("2d"),
-		c = f.width,
-		h = f.height,
-		b = p.createLinearGradient(0, 0, 0, h),
-		b2 = p2.createLinearGradient(0, 0, 0, h),
-		q = 1, //柱宽
-		s = 2, //柱间距
-		e = c / (q + s) | 0,
-		d = l / e | 0,
-		o;
-		// b.addColorStop(0, "#85c2ff");
-		// b.addColorStop(0.7, "#5c87b2");
-		b2.addColorStop(0, "rgba(57,156,255,0)");
-		b2.addColorStop(0.5, "rgba(28,78,128,0)");
-		// b.addColorStop(0.5, "#F00");
-		b.addColorStop(1, "#000");
-		b2.addColorStop(1, "#000");
-		p.fillStyle = b;
-		p2.fillStyle = b2;
-		k.connect(r);
-		r.connect(n.destination);
-		function m() {
+	//动画，类似setTimeout的递归调用实现动画
+	window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;//j
+
+		var contex = new AudioContext(),  //创建一个audio上下文
+		analyser = contex.createAnalyser(), //r 创建一个分析节点analyser 能对信号进行频域和时域上的分析
+		oAudio = new Audio(window.location.search.substring(1) || mp3), //t 创建一个audio元素
+		source = contex.createMediaElementSource(oAudio), //k 创建音频源
+		frequency = analyser.frequencyBinCount / 2,  //analyser.frequencyBinCount 取样频率 默认1024  fftSize 是指频率分析下的快速傅里叶变换大小，他的值被限定在 32-2048 的 2 的整数次方
+		u8arr = new Uint8Array(frequency),  //长度为frequency的Uint8Array字节数组
+		aContex2D = [aCanvas[0].getContext("2d"),aCanvas[1].getContext("2d"),aCanvas[2].getContext("2d"),aCanvas[3].getContext("2d")],
+
+
+
+		width = aCanvas[0].width,
+		height = aCanvas[0].height,
+		aBarGradient = [aContex2D[0].createLinearGradient(0, 0, 0, height),aContex2D[1].createLinearGradient(0, 0, 0, height),aContex2D[2].createLinearGradient(0, 0, 0, height),aContex2D[3].createLinearGradient(0, 0, 0, height)],
+		barWidth = 6, //柱宽 q
+		barMargin = 6, //柱间距
+		barNumber = width / (barWidth + barMargin) | 0, //柱个数
+		frequencyLength = frequency / barNumber | 0;  //frequency对应到柱个数的步长
+		for(var i=0; i<aBarGradient.length; i++){
+			aBarGradient[i].addColorStop(0, "#369");
+			aBarGradient[i].addColorStop(1, "#000");
+			aContex2D[i].fillStyle = aBarGradient[i];
+		}
+		source.connect(analyser);  //节点.连接到（目标节点）  当目标节点是 contex.destination 就可以输出了
+		analyser.connect(contex.destination); //destination 是 AudioContext 实例的固有属性，他就是信号的最终汇聚的位置，也是信号的输出位置
+		function draw() {
 			
-			o = 10000;
-			if (t.paused || t.ended) {
+			if (oAudio.paused || oAudio.ended) {
 				return
 			}
-			r.getByteFrequencyData(u);
-			p.clearRect(0, 0, c, h);
-			p2.clearRect(0, 0, c, h);
-			for (o = 0; o < e; o++) {
-				p.fillRect(o * (q + s), Math.max(2, h - u[o * d] * 0.2), q, h)
-				p2.fillRect(o * (q + s), Math.max(2, h - u[o * d] * 0.2), q, h)
+			analyser.getByteFrequencyData(u8arr); //频域
+			// analyser.getByteTimeDomainData(u8arr);  //时域
+			aContex2D[0].clearRect(0, 0, width, height);  //清除矩形
+			aContex2D[1].clearRect(0, 0, width, height);
+			aContex2D[2].clearRect(0, 0, width, height);
+			aContex2D[3].clearRect(0, 0, width, height);
+			for (var i = 0; i < barNumber; i++) {
+				var p1 = i * (barWidth + barMargin),
+					p2 = Math.max(2, height - u8arr[i * frequencyLength] * 0.5);
+				aContex2D[0].fillRect(p1, p2, barWidth, height);  //填充矩形 参数：x,y,width,height
+				aContex2D[1].fillRect(p1, p2, barWidth, height);
+				aContex2D[2].fillRect(p1, p2, barWidth, height);
+				aContex2D[3].fillRect(p1, p2, barWidth, height);
 			}
-			j(m)
+			requestAnimationFrame(draw);
 		}
-		t.addEventListener("playing", m, false);
-		t.addEventListener("play", function(){
+		oAudio.addEventListener("playing", draw, false);
+		oAudio.addEventListener("play", function(){
 			$('#bg').addClass('on');
-			$('#canvas').addClass('on');
+			$('#show').addClass('on');
 		}, false);
-		t.addEventListener("ended",
-		function() {
-			p.clearRect(0, 0, c, h);
-			p.fill()
-		},
-		false);
-		// t.setAttribute("controls", "controls");
-		t.autoplay = true;
-		document.getElementById("show").appendChild(t)
-	}
+		oAudio.addEventListener("ended",function() {
+			// aContex2D[0].clearRect(0, 0, width, height);
+			// aContex2D[0].fill();
+		},false);
+		oAudio.autoplay = true;
+		oAudio.loop = true;
+})([
+	document.querySelector("#canvas"),
+	document.querySelector("#canvas2"),
+	document.querySelector("#canvas3"),
+	document.querySelector("#canvas4")
+],'03.mp3');
--- work/MangoPlayer/music2.js
+++ work/MangoPlayer/music2.js
@@ -1,52 +1,73 @@
-(function(a) {
+(function(){
+	//audio 上下文
 	window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext;
+	if(!AudioContext){return;}
+
+	//动画，类似setTimeout的递归调用实现动画
+	window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;//j
+
+
 	if (typeof window.AudioContext == "undefined") {
 		document.getElementById("show").innerHTML = '<object id="player" classid="CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6"> <param name="url" value="' + a + '.mp3" /><param name="AutoStart" value="1" /><embed src="' + a + '.mp3" type="application/x-mplayer2" width="360" height="60" autostart="true"></embed></object>'
 	} else {
-		var n = new AudioContext(),
-		r = n.createAnalyser(),
-		t = new Audio("http://www.wuweierwei.com/music/a.mp3"),
-		k = n.createMediaElementSource(t),
-		j = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame,
-		l = r.frequencyBinCount / 2,
-		u = new Uint8Array(l),
-		f = document.querySelector("#show>canvas"),
-		p = f.getContext("2d"),
-		c = f.width,
-		h = f.height,
-		b = p.createLinearGradient(0, 0, 0, h),
-		q = 2,
-		s = 1,
-		e = c / (q + s) | 0,
-		d = l / e | 0,
-		o;
-		b.addColorStop(0, "#FF0");
-		b.addColorStop(0.5, "#F00");
-		b.addColorStop(1, "#000");
-		p.fillStyle = b;
-		k.connect(r);
-		r.connect(n.destination);
-		function m() {
-			o = 10000;
-			if (t.paused || t.ended) {
+		var contex = new AudioContext(),  //创建一个audio上下文
+		analyser = contex.createAnalyser(), //r 创建一个分析节点analyser 能对信号进行频域和时域上的分析
+		// oAudio = new Audio("http://www.wuweierwei.com/music/a.mp3"),
+		oAudio = new Audio("01.mp3"), //t 创建一个audio元素
+		source = contex.createMediaElementSource(oAudio), //k 创建音频源
+		frequency = analyser.frequencyBinCount / 2,  //analyser.frequencyBinCount 取样频率 默认1024  fftSize 是指频率分析下的快速傅里叶变换大小，他的值被限定在 32-2048 的 2 的整数次方
+
+		u8arr = new Uint8Array(frequency),  //长度为frequency的Uint8Array字节数组
+		oCanvas = document.querySelector("#canvas"),
+		oCanvas2 = document.querySelector("#canvas2"),
+		contex2D = oCanvas.getContext("2d"),
+		contex2D2 = oCanvas2.getContext("2d"),
+		width = oCanvas.width,  //c
+		height = oCanvas.height,  //h
+		barGradient = contex2D.createLinearGradient(0, 0, 0, height),  //线性渐变 作为 strokeStyle 或 fillStyle 属性的值， 使用addColorStop() 创建不同颜色 参数（渐变开始点x坐标,开始y,结束x,结束y）
+		barGradient2 = contex2D2.createLinearGradient(0, 0, 0, height),
+		barWidth = 1, //柱宽 q
+		barMargin = 2, //柱间距
+		barNumber = width / (barWidth + barMargin) | 0, //柱个数
+		frequencyLength = frequency / barNumber | 0;  //frequency对应到柱个数的步长
+		// b.addColorStop(0, "#85c2ff");
+		// b.addColorStop(0.7, "#5c87b2");
+		barGradient2.addColorStop(0, "rgba(57,156,255,0)");
+		barGradient2.addColorStop(0.5, "rgba(28,78,128,0)");
+		// b.addColorStop(0.5, "#F00");
+		barGradient.addColorStop(1, "#000");
+		barGradient2.addColorStop(1, "#000");
+		contex2D.fillStyle = barGradient;
+		contex2D2.fillStyle = barGradient2;
+		source.connect(analyser);  //节点.连接到（目标节点）  当目标节点是 contex.destination 就可以输出了
+		analyser.connect(contex.destination); //destination 是 AudioContext 实例的固有属性，他就是信号的最终汇聚的位置，也是信号的输出位置
+		function draw() {
+			
+			if (oAudio.paused || oAudio.ended) {
 				return
 			}
-			r.getByteFrequencyData(u);
-			p.clearRect(0, 0, c, h);
-			for (o = 0; o < e; o++) {
-				p.fillRect(o * (q + s), Math.max(2, h - u[o * d] * 0.5), q, h)
+			analyser.getByteFrequencyData(u8arr); //频域
+			// analyser.getByteTimeDomainData(u8arr);  //时域
+			contex2D.clearRect(0, 0, width, height);  //清除矩形
+			contex2D2.clearRect(0, 0, width, height);
+			for (var i = 0; i < barNumber; i++) {
+				contex2D.fillRect(i * (barWidth + barMargin), Math.max(2, height - u8arr[i * frequencyLength] * 0.2), barWidth, height)  //填充矩形 参数：x,y,width,height
+				contex2D2.fillRect(i * (barWidth + barMargin), Math.max(2, height - u8arr[i * frequencyLength] * 0.2), barWidth, height)
 			}
-			j(m)
+			requestAnimationFrame(draw);
 		}
-		t.addEventListener("playing", m, false);
-		t.addEventListener("ended",
-		function() {
-			p.clearRect(0, 0, c, h);
-			p.fill()
-		},
-		false);
-		t.setAttribute("controls", "controls");
-		t.autoplay = true;
-		document.getElementById("show").appendChild(t)
+		oAudio.addEventListener("playing", draw, false);
+		oAudio.addEventListener("play", function(){
+			$('#bg').addClass('on');
+			$('#canvas').addClass('on');
+		}, false);
+		oAudio.addEventListener("ended",function() {
+			contex2D.clearRect(0, 0, width, height);
+			contex2D.fill();
+		},false);
+		// t.setAttribute("controls", "controls");
+		oAudio.autoplay = true;
+		oAudio.loop = true;
+		// document.getElementById("show").appendChild(oAudio)
 	}
-})(location.search.replace("?", ""));
+})()
